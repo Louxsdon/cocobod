f5a68c38f1905b749d6b72e056f89aa461ead892
louxsdon <louisayivi.dev@gmail.com>
2023-06-30 22:42:52 +0000

    refactor

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::prefix('/auth')->group(function () {
 });
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('auth/Login', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
