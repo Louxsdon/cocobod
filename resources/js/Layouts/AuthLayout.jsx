6b926eaa250ead951fe638aec323bb41de62d850
louxsdon <louisayivi.dev@gmail.com>
2023-07-06 13:22:52 +0000

    include flash message

--- resources/js/Layouts/AuthLayout.jsx
+++ resources/js/Layouts/AuthLayout.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "@/Components/Navbars/AuthNavbar.jsx";
 import FooterSmall from "@/Components/Footers/FooterSmall.jsx";
+import { ToastContainer, toast } from "react-toastify";
+import { usePage } from "@inertiajs/react";
 
 // views
 
 export default function AuthLayout({ children }) {
+    const { flash } = usePage().props;
+    useEffect(() => {
+        if (flash.message) {
+            toast(flash.message.text, {
+                type: flash.message.status ? flash.message.status : "success",
+                theme: "colored",
+            });
+        }
+    }, [flash.message]);
     return (
         <>
+            <ToastContainer />
             {/* <Navbar transparent /> */}
             <main>
                 <section className="relative w-full h-full py-40 min-h-screen">
